59c01f5cb718d77f76e100ee980ed6888c8d3696
Dani <danielferrert10@gmail.com>
2025-02-25 19:23:06 +0100

    Enemigo casi terminado (hay que hacer sistema de vida)

--- Assets/Scripts/MosquitoController.cs
+++ Assets/Scripts/MosquitoController.cs
@@ -5,46 +5,77 @@ using UnityEngine;
 
 public class MosquitoController : MonoBehaviour
 {
-    public GameObject pointA;
-    public GameObject pointB;
-    private Rigidbody2D rb;
-    private Animator anim;
-    private Transform currentPoint;
     public float speed;
-    private SpriteRenderer sprite;
+    public GameObject player;
+    public bool chase = false;
+    public Transform startingPoint;
+
+    Rigidbody2D rb;
+    CapsuleCollider2D collider;
+
+    public bool attacking;
+    Vector2 suma;
 
     // Start is called before the first frame update
     void Start()
     {
+        chase = false;
+        suma = new Vector2(1, 1);
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-        currentPoint = pointB.transform;
-        sprite = GetComponent<SpriteRenderer>();
+        collider = GetComponent<CapsuleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 point = currentPoint.position - transform.position;
-        if(currentPoint == pointB.transform)
+        if(chase == true && !attacking)
         {
-            rb.velocity = new Vector2(speed, 0);
-            sprite.flipX = true;
+            Chase();
+        }
+        else if (chase == false && !attacking)
+        {
+            ReturnStart();
+        }
+        Flip();
+
+        if (attacking == true)
+        {
+            Attack();
+        }
+    }
+
+    void ReturnStart()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, startingPoint.position, speed * Time.deltaTime);
+    }
+
+    void Chase()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+
+    void Attack()
+    {
+        chase = false;
+        rb.bodyType = RigidbodyType2D.Static;
+        collider.isTrigger = true;
+        transform.position = new Vector2(player.transform.position.x +2, player.transform.position.y +2);
+    }
+
+    void Flip()
+    {
+        if(transform.position.x > player.transform.position.x)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
         }
         else
         {
-            rb.velocity = new Vector2(-speed, 0);
-            sprite.flipX = false;
+            transform.rotation = Quaternion.Euler(0, 180, 0);
         }
+    }
 
-        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
-        {
-            currentPoint = pointA.transform;
-        }
-
-        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
-        {
-            currentPoint = pointB.transform;
-        }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        attacking = true;
     }
 }
